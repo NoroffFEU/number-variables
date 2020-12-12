a7abd354c112a28c942d00bfe1f1ad2d8c388896
Connor O'Brien <connor@hifranc.com>
2020-12-12 20:31:24 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// + addition
+// - subtraction
+// * multiplication
+// / division
+// % remainder operator
+
+var number1 = 5;
+var number2 = "fff";
+
+var result = number1 / number2;
+
+// check if a variable value is a number
+var isNumber = isNaN(result);
+
+console.log(isNumber);
+
+var number3 = 4;
+var number4 = 2;
+
+// get the remainder of dividing two numbers
+var remainder = number3 % number4;
+
+console.log(remainder);
